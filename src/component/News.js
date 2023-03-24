984d80b691aab737a7e6797694ccba073ececf80
HAMMAD <108028597+HAMMAD-DEVELOPER-TECH@users.noreply.github.com>
2023-03-25 00:58:40 +0500

    Fetching News from API and populating in a with array map method

---
+++ src/component/News.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+
+export class News extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+    };
+  }
+
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=8daa90c4fa884622a121f47224b2fd9a";
+    let data = await fetch(url);
+    let parseData = await data.json();
+    console.log(parseData);
+    this.setState({ articles: parseData.articles });
+  }
+
+  render() {
+    return (
+      <div className="container my-3">
+        <h1>PBC - Top Headlines</h1>
+        <div className="row">
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
+                  imgUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default News;
--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -7,11 +7,24 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card">
-          <img src={imgUrl} className="card-img-top" alt="..." />
+          <img
+            src={
+              !imgUrl
+                ? "https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2023/03_mar/23_thu/img_1679588088930_23.jpg?"
+                : imgUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsDetails/" className="btn btn-sm btn-warning">
+            <a
+              href={newsUrl}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-sm btn-warning"
+            >
               Read More
             </a>
           </div>
