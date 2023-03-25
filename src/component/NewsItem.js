70cd16907e7711e4b07660d88a50e5252151163d
HAMMAD <108028597+HAMMAD-DEVELOPER-TECH@users.noreply.github.com>
2023-03-25 23:41:20 +0500

    Converting two function to only on Update Function and changing the color of small author and date text

--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -9,7 +9,7 @@ export class NewsItem extends Component {
       <div className="my-3">
         <div className="card">
           <span
-            class="position-absolute top-0 translate-middle badge rounded-pill bg-info"
+            className="position-absolute top-0 translate-middle badge rounded-pill bg-info"
             style={{ zIndex: "1", left: "90%" }}
           >
             {source}
@@ -27,7 +27,7 @@ export class NewsItem extends Component {
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
             <p className="card-text">
-              <small className="text-body-secondary">
+              <small className="text-primary">
                 By {!author ? "Unknown" : author} on{" "}
                 {new Date(date).toGMTString()}
               </small>
