783fd5ebb76d8e715298fe56fc0603dffa0b244b
HAMMAD <108028597+HAMMAD-DEVELOPER-TECH@users.noreply.github.com>
2023-03-25 22:50:51 +0500

    Adding CSS like Author Name and Time of publish and Badges at the top which referr to the NEWS Channel

--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -2,11 +2,18 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imgUrl, newsUrl } = this.props;
+    let { title, description, imgUrl, newsUrl, author, date, source } =
+      this.props;
 
     return (
       <div className="my-3">
         <div className="card">
+          <span
+            class="position-absolute top-0 translate-middle badge rounded-pill bg-info"
+            style={{ zIndex: "1", left: "90%" }}
+          >
+            {source}
+          </span>
           <img
             src={
               !imgUrl
@@ -19,6 +26,12 @@ export class NewsItem extends Component {
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-body-secondary">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
             <a
               href={newsUrl}
               target="_blank"
